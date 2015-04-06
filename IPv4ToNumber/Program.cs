44db6165fb291c0c4c0ea359c06904eac06b906a
Eyal Shilony <git@shilony.net>
2015-04-06 23:08:58 +0300

    Initial commit.

---
+++ IPv4ToNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace IPv4ToNumber
+{
+	using System;
+
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			// 255.255.255.255 = (2^32)-1 = uint
+
+			long a = 255 * (long)Math.Pow(256, 3);	// Class A
+			long b = 255 * (long)Math.Pow(256, 2);	// Class B
+			long c = 255 * 256; 					// Class C		
+			long d = 255; 							// Class D
+
+			long ip = a + b + c + d;
+
+			Console.WriteLine(ip);
+
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+IPv4ToNumber
+============
+
+An example that demonstrates how to convert IP into number.
+
+### Application Type ###
+* Console application
+
+### Dependencies ###
+* .NET 2.0
